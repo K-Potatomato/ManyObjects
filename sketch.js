490af26d23bbcdb66a7873e46b739c8a7b2a8eca
madey <89770729+K-Potatomato@users.noreply.github.com>
2021-11-02 15:30:45 +0900

    Manyball
    Manyball

--- sketch.js
+++ sketch.js
@@ -1,14 +1,21 @@
-let m;
-let m2;
-
 function setup() {
   createCanvas(400, 400);
   m = new Mover(50, 50, 10);
   m2 = new Mover(300,300,50);
+  for (let i = 0; i <5; i++){
+    let mover = new Mover(random(400),random(400),random(5, 10));
+    movers.push(mover);
+  }
 }
 
 function draw() {
   background(220);
+
+  for( let mover of movers){
+    mover.update();
+    mover.show();
+  }
+
   m.update();
   m.show();
   m2.update();
@@ -17,9 +24,12 @@ function draw() {
   if (mouseIsPressed) {
     //make attract to mouse position
     let mousePos = createVector(mouseX, mouseY);
+
+    for( let mover of movers){
+     mover.attractTo(mousePos);
+    }
     m.attractTo(mousePos);
     m2.attractTo(mousePos);
-
     // let f = p5.Vector.sub(mousePos, m.pos);
     // f.limit(2);
     // m.applyForce(f);
